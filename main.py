b04bf5d855ab0f62f02e32806afa6de817e6cda8
Giuseppe-Andrei <giuseppeandrei.guarino@wvsu.edu.ph>
2026-09-07 17:29:25 +0800

    Add docstrings to greet and add functions

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 def greet(name):
+    """Basic Greeting Function"""
     print(f"Hello, {name}!")
 
 greet("World")
 
 def add(a,b):
+    """Additon Function"""
     c = a + b
     print(c)
 
 add(3,5)
+
